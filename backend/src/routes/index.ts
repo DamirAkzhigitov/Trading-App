0e9ef7496dbbc57bf416db473d8af254a0e93c81
DamirAkzhigitov <damir.ckelet@gmail.com>
2022-06-18 14:51:06 +0300

    read csv file and send to frontend by WS

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,15 +1,38 @@
 import { FastifyInstance } from 'fastify'
+import { readExcel } from '../utils/xlsxReader'
+
+const timer = (ms: number) => new Promise(res => setTimeout(res, ms))
+
+interface CellItem {
+  DATE: number,
+  TIME: unknown,
+  LAST: number,
+  VOLUME: number,
+  FLAGS: number
+}
 
 async function routes(fastify: FastifyInstance) {
-  // onRoomChanged(() => {
-  //   fastify.websocketServer.clients.forEach((client: any) => {
-  //     if (client.readyState === 1) {
-  //       client.send(JSON.stringify(roomListProxy))
-  //     }
-  //   })
-  //
-  //   // connection.socket.send()
-  // })
+  console.log('start main rouad');
+
+  const ticks = readExcel()
+
+  const emulateResponse = async (ticks: unknown[]) => {
+    for await (const tick of ticks) {
+      // console.log(tick)
+      fastify.websocketServer.clients.forEach((client: any) => {
+        if (client.readyState === 1) {
+          client.send(JSON.stringify(tick))
+        }
+      })
+      await timer(100)
+    }
+  }
+
+  emulateResponse(ticks)
+
+
+    // console.log('set socket');
+
 
   fastify.get('/', { websocket: true }, () => {
     //
